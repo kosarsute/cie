b9a7ed280295776192d6abe404059e01adb1a34b
kosarsute <kosarsute@gmail.com>
2022-03-10 17:11:31 -0800

    changes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-1. To install all modules run: npm i
-2. To run all tests run: npm test
+1. User should be able to login with valid username and password  
+2. User should not be able to login with valid username and invalid password 
+3. User should not be able to login with invalid username and valid password 
+2. User should be able to login by biometrics 
+1. Verify message for invalid login (Password or email) 
+2. Verify that you can login in iOS and android mobile platform with valid username and password 
+3. Verify that user cannot enter characters more then the specified range in each field (Password or email) 
+4. Verify if user can login in the same time in iOS and android using same credentials
+5. Verify if the login page fits the mobile screen. The user should not have to scroll the screen.
+10. Verify that login button in clickable. 
+11. Verify that biometrics button is clicable
+
--- specs/login/login-spec.js
+++ specs/login/login-spec.js
@@ -1,8 +1,5 @@
 const Login = require('../../page_objects/login-page');
-const Appointment = require('../../page_objects/appointment-page');
 const Credentials = require('../../data/credentials.json');
-
-const { expect } = require("chai");
 
 const email = Credentials.admin.login.email;
 const password = Credentials.admin.login.password;
@@ -11,35 +8,9 @@ describe('Login', () => {
     beforeEach(function () {
         browser.url('./')
     });
-
     it('should be able to login with matching credentials', () => {
         // User should be able to login
         Login.login({email: email, password: password});
-        browser.waitUntil(() => {
-            return Login.$$header.map((elem) => elem.isDisplayed()).length > 3;
-        }, { timeout: 10000, timeoutMsg:'Three elements were not visible'});
-        // Verify phone number, email adress, address and birthday
-        const array = Login.$$header.map(text=>text.getText())
-        expect(array).to.contain('6157565765')
-        expect(array).to.contain('testpatient107@mailinator.com')
-        expect(array).to.contain('123 Pacific Dr Fullerton, CA')
-        expect(Login.$ageHeader.getText()).to.equal('Nov 11, 2000 | Male')
-        // Schedule appointment for Round 1
-        Appointment.$scheduleVaccineAppointmentBtn.click()
-        //Select date for appointment
-        Appointment.SelectAppointmentDate('Jan 28, 2022')
-        //Select time for appointment
-        Appointment.SelectAppointmentTime('query_builder 1:00 AM')
-        // Click on schedule appointment
-        Appointment.$scheduleAppointmentBtn.click()
-        // Close confirmation button 
-        Appointment.$closeConfirmationBtn.click()
-        // Cancel appointment and verify it
-        Appointment.$cancelAppointmentBtn.click()
-        Appointment.$confirmCancelAppointmentBtn.click()
-        browser.waitUntil(() => {
-            return Appointment.$scheduleAppointmentBtn.isDisplayed();
-        }, { timeout: 10000, timeoutMsg:'Schedule appointment button did not show up after 10 seconds'});
     })
 })
 
